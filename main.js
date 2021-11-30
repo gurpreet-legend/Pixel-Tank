c86829d3d70674c33ebbe1b52c8afe72da08cb23
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-30 19:21:51 +0530

    folder structure updated

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x48dbfb )
+scene.background = new THREE.Color(0x48dbfb)
 
 
 // Camera
-const camera = new THREE.PerspectiveCamera( 
-    40, 
-    window.innerWidth / window.innerHeight, 
-    1, 
-    1000 
-  ); // props(viewing frustum) : FOV, Aspect ratio, near plane, far plane
-camera.position.set( 0, 2, 5);
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+); // props(viewing frustum) : FOV, Aspect ratio, near plane, far plane
+camera.position.set(0, 2, 5);
 
 
 
@@ -23,16 +23,16 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true, // to minimize the distortions while rendering our 3D model
   alpha: true //to make threeJS background transparent
 });
-renderer.setSize( innerWidth, innerHeight );
+renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 renderer.outputEncoding = THREE.sRGBEncoding  //////AMAZING THING
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
 
 
 
 //Orbit controls
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
@@ -44,28 +44,28 @@ controls.enableZoom = true;
 //GLTF Loader
 let model;
 const loader = new GLTFLoader();
-loader.load( './model/scene.gltf', function ( gltf ) {  // PROPS :(url : String, onLoad : Function, onProgress : Function, onError : Function )
-    model = gltf.scene;
-    console.log(model)
-    model.castShadow = true;
-    model.receiveShadow = true;
-	  model.position.set( 0, -0.5, 0 );
-	  model.scale.set( 0.01, 0.01, 0.01 );
-	  scene.add(model);
+loader.load('/scene.gltf', function (gltf) {  // PROPS :(url : String, onLoad : Function, onProgress : Function, onError : Function )
+  model = gltf.scene;
+  console.log(model)
+  model.castShadow = true;
+  model.receiveShadow = true;
+  model.position.set(0, -0.5, 0);
+  model.scale.set(0.01, 0.01, 0.01);
+  scene.add(model);
 
-    
 
-    model.traverse(function(child) {
 
-      if (child.isMesh) {
-         child.material.wireframe = false;
-         child.material.flatSahding = false;
-      }
-    })
+  model.traverse(function (child) {
 
-    renderer.render(scene, camera)
+    if (child.isMesh) {
+      child.material.wireframe = false;
+      child.material.flatSahding = false;
+    }
+  })
 
-    animate();
+  renderer.render(scene, camera)
+
+  animate();
 
 })
 
@@ -75,20 +75,20 @@ window.onresize = function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
 };
 
 
 // Animate
-const animate = function(){
-  requestAnimationFrame( animate );
+const animate = function () {
+  requestAnimationFrame(animate);
 
-  controls.update(); 
+  controls.update();
 
   model.rotation.y += 0.005;
 
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 };
 
 
